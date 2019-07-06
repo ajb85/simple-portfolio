2559403ab6b058cfa06eaf848909e2727e8449e9
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-06 18:11:20 -0400

    Moves 'Header' into 'Sections' folder

---
+++ src/components/Sections/Header/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import headerIMG from "img/backgrounds/rainier_bright.jpg";
+
+function Header() {
+  return (
+    <header>
+      <div className={styles.headerIMG}>
+        <img src={headerIMG} alt="Mt. Rainier in June 2014" />
+        <div className={styles.quote}>
+          <p>
+            ‘You can, you should, and if you’re brave enough to start, you will’
+          </p>
+          <p>-Stephen King</p>
+        </div>
+      </div>
+      <div className={styles.profileContainer}>
+        <div className={styles.profile}>
+          <div className={styles.title}>
+            <h1>AJ Brush</h1>
+            <h2>Full Stack Web</h2>
+            <p>
+              Developer with a love for React and a passion for good training
+              and management
+            </p>
+          </div>
+          <a href="#about" className={styles.button}>
+            <button>About Me</button>
+          </a>
+          <div className={styles.socialLinks}>
+            <a
+              href="https://www.linkedin.com/in/andrew-brush-58205b122/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin" />
+            </a>
+            <a
+              href="https://github.com/ajb85"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github-square" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
