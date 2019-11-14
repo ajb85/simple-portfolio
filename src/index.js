f522e107becfa723990b626a4bbda1dad2e31e47
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-14 18:37:54 -0500

    Cleans up code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+
+import App from './App.js';
+
+import { createBrowserHistory } from 'history';
 
 ReactDOM.render(
   <Router history={createBrowserHistory()}>
     <App />
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
